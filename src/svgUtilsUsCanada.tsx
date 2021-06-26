b90c3a5a496dff2f2ac89b6b61501a55185ccc48
Ron Veenstra <ron-git@agathongroup.com>
2021-06-25 22:34:41 -0400

    New sag fill

--- src/svgUtilsUsCanada.tsx
+++ src/svgUtilsUsCanada.tsx
@@ -5,9 +5,9 @@ export default function drawDetails() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
-      width="1114"
-      height="1942"
-      viewBox="-24500 -27050 55700 47100"
+      width="1203"
+      height="1270"
+      viewBox="0 0 700.4 660"
     >
       <title>Map of USA and Canada</title>
       <style type="text/css">{}</style>
@@ -15,13 +15,10 @@ export default function drawDetails() {
         <mask id="all">
           <g>
             <path
-              fill="#fff"
+              fill="#000"
               stroke="none"
               d="m-24500-27050h55700v47100h-55700z"
             />
-            <g fill="none" stroke="#000" strokeWidth="40">
-              <path d="m17072 12065 28-30 64-131 127-130-1-96 54-71 63-32 25-62 28-33 19-59 73 5 56-12 285-249 16-46v-43" />
-            </g>
           </g>
         </mask>
       </defs>
