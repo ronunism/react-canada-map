454cc0a7df4f13b61169f6a18bb7ad5e7a7ff6a7
Ron Veenstra <ron-git@agathongroup.com>
2021-06-27 23:05:01 -0400

    Simplify Text on US Map

--- src/svgUtilsUsCanada.tsx
+++ src/svgUtilsUsCanada.tsx
@@ -3,10 +3,10 @@ import React from "react"
 export default function drawDetails() {
   return (
     <>
-    <text x="635" y="555" text-anchor="middle" font-family="Arial, sans-serif" font-size="12px" stroke="none" fill="#333333" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;" font-weight="bold" stroke-width="1"><tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">DC</tspan></text>
-    <text x="635" y="575" text-anchor="middle" font-family="Arial, sans-serif" font-size="12px" stroke="none" fill="#333333" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;" font-weight="bold" stroke-width="1"><tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">GOV</tspan></text>
-    <text x="635" y="595" text-anchor="middle" font-family="Arial, sans-serif" font-size="12px" stroke="none" fill="#333333" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;" font-weight="bold" stroke-width="1"><tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">DIP</tspan></text>
-    <text x="635" y="615" text-anchor="middle" font-family="Arial, sans-serif" font-size="12px" stroke="none" fill="#333333" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;" font-weight="bold" stroke-width="1"><tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">CON</tspan></text>
+    <text x="635" y="555" font-family="Arial, sans-serif" font-size="12px" fill="#333333" font-weight="bold">DC</text>
+    <text x="635" y="575" font-family="Arial, sans-serif" font-size="12px" fill="#333333" font-weight="bold">GOV</text>
+    <text x="635" y="595" font-family="Arial, sans-serif" font-size="12px" fill="#333333" font-weight="bold">DIP</text>
+    <text x="635" y="615" font-family="Arial, sans-serif" font-size="12px" fill="#333333" font-weight="bold">CON</text>
     </>
   )
 }
