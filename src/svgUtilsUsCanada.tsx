a46784b20bbd214baec8984119dadc29ab0b3d4c
Ron Veenstra <ron-git@agathongroup.com>
2022-05-14 15:48:43 -0400

    fontSize

--- src/svgUtilsUsCanada.tsx
+++ src/svgUtilsUsCanada.tsx
@@ -3,10 +3,10 @@ import React from "react"
 export default function drawDetails() {
   return (
     <>
-    <text x="600" y="580" font-size="13px" fill="#333333">Washington DC</text>
-    <text x="600" y="600" font-size="13px" fill="#333333">US Government</text>
-    <text x="600" y="620" font-size="13px" fill="#333333">Diplomat</text>
-    <text x="600" y="640" font-size="13px" fill="#333333">Consul</text>
+    <text x="600" y="580" fontSize="13px" fill="#333333">Washington DC</text>
+    <text x="600" y="600" fontSize="13px" fill="#333333">US Government</text>
+    <text x="600" y="620" fontSize="13px" fill="#333333">Diplomat</text>
+    <text x="600" y="640" fontSize="13px" fill="#333333">Consul</text>
     </>
   )
 }
